dfe891e7a154e8ec7f7616071fb2cacb79367b41
Arif Hossain <hossaininfo@ymail.com>
2023-02-19 13:46:17 +0100

    added a maven project with dependency.

---
+++ data_persistence_and_access_postgresql/src/main/java/com/example/data_persistence_and_access_postgresql/DataPersistenceAndAccessPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.data_persistence_and_access_postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPersistenceAndAccessPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPersistenceAndAccessPostgresqlApplication.class, args);
+	}
+
+}
---
+++ data_persistence_and_access_postgresql/src/test/java/com/example/data_persistence_and_access_postgresql/DataPersistenceAndAccessPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data_persistence_and_access_postgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPersistenceAndAccessPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
