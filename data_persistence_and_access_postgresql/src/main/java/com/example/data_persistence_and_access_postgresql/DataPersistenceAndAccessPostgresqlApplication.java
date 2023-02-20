610a0cb4254ba95583810c8ac4ef1e0de91ebfa0
Arif Hossain <hossaininfo@ymail.com>
2023-02-21 00:07:12 +0100

    made a menu to play on console

--- data_persistence_and_access_postgresql/src/main/java/com/example/data_persistence_and_access_postgresql/DataPersistenceAndAccessPostgresqlApplication.java
+++ data_persistence_and_access_postgresql/src/main/java/com/example/data_persistence_and_access_postgresql/DataPersistenceAndAccessPostgresqlApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -23,14 +24,82 @@ public class DataPersistenceAndAccessPostgresqlApplication implements CommandLin
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.print("Enter the Customer ID: ");
-		int id = scanner.nextInt();
-		scanner.nextLine();
-		Customer customer = customerService.findById(id);
-		if (customer != null) {
-			System.out.println(customer);
-		} else {
-			System.out.println("Customer not found");
+		int choice = -1;
+		while (choice != 0) {
+			System.out.println("Select an option:");
+			System.out.println("1. List all customers");
+			System.out.println("2. Find customer by ID");
+			System.out.println("3. Find customer by name");
+			System.out.println("4. List customers with limit and offset");
+			System.out.println("5. Create a new customer");
+			System.out.println("6. Update an existing customer");
+			System.out.println("7. Delete a customer");
+			System.out.println("0. Exit");
+			System.out.print("Enter your choice: ");
+
+			choice = scanner.nextInt();
+			scanner.nextLine(); // consume newline
+
+			switch (choice) {
+				case 1:
+					List<Customer> customers = customerService.findAll();
+					customers.forEach(System.out::println);
+					break;
+				case 2:
+					System.out.print("Enter customer ID: ");
+					int id = scanner.nextInt();
+					Customer customer = customerService.findById(id);
+					System.out.println(customer);
+					break;
+				case 3:
+					System.out.print("Enter customer name: ");
+					String name = scanner.nextLine();
+					customer = customerService.findByName(name);
+					System.out.println(customer);
+					break;
+				case 4:
+					System.out.print("Enter limit: ");
+					int limit = scanner.nextInt();
+					System.out.print("Enter offset: ");
+					int offset = scanner.nextInt();
+					customers = customerService.findAllWithLimit(limit, offset);
+					customers.forEach(System.out::println);
+					break;
+				case 5:
+					System.out.print("Enter first name: ");
+					String firstName = scanner.nextLine();
+					System.out.print("Enter last name: ");
+					String lastName = scanner.nextLine();
+					System.out.print("Enter company: ");
+					String company = scanner.nextLine();
+					System.out.print("Enter address: ");
+					String address = scanner.nextLine();
+					System.out.print("Enter city: ");
+					String city = scanner.nextLine();
+					System.out.print("Enter state: ");
+					String state = scanner.nextLine();
+					System.out.print("Enter country: ");
+					String country = scanner.nextLine();
+					System.out.print("Enter postal code: ");
+					String postalCode = scanner.nextLine();
+					System.out.print("Enter phone: ");
+					String phone = scanner.nextLine();
+					System.out.print("Enter fax: ");
+					String fax = scanner.nextLine();
+					System.out.print("Enter email: ");
+					String email = scanner.nextLine();
+					Customer newCustomer = new Customer(0, firstName, lastName, company, address, city, state, country, postalCode, phone, fax, email);
+					customerService.save(newCustomer);
+					System.out.println("New customer created with ID " + newCustomer.id());
+					break;
+				case 6:
+					System.out.print("Enter customer ID: ");
+					id = scanner.nextInt();
+					scanner.nextLine(); // consume newline
+					customer = customerService.findById(id);
+
+					break;
+			}
 		}
 	}
 }
---
+++ data_persistence_and_access_postgresql/src/main/java/com/example/data_persistence_and_access_postgresql/menu/ConsoleMenu.java
@@ -0,0 +1,4 @@
+package com.example.data_persistence_and_access_postgresql.menu;
+
+public class ConsoleMenu {
+}
