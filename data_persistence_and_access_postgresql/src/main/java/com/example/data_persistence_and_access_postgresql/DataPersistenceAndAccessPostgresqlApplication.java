0ed2e9b64520ec44b8c904bb52e9a486074bf305
Arif Hossain <hossaininfo@ymail.com>
2023-02-20 23:41:38 +0100

    implemented CommandLineRunner to run services

--- data_persistence_and_access_postgresql/src/main/java/com/example/data_persistence_and_access_postgresql/DataPersistenceAndAccessPostgresqlApplication.java
+++ data_persistence_and_access_postgresql/src/main/java/com/example/data_persistence_and_access_postgresql/DataPersistenceAndAccessPostgresqlApplication.java
@@ -1,16 +1,36 @@
 package com.example.data_persistence_and_access_postgresql;
 
-import com.example.data_persistence_and_access_postgresql.connection.PostgradDAO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
+import com.example.data_persistence_and_access_postgresql.model.Customer;
+import com.example.data_persistence_and_access_postgresql.service.CustomerService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
-public class DataPersistenceAndAccessPostgresqlApplication {
+public class DataPersistenceAndAccessPostgresqlApplication implements CommandLineRunner {
+	private static final Scanner scanner = new Scanner(System.in);
+	private final CustomerService customerService;
+
+	public DataPersistenceAndAccessPostgresqlApplication(CustomerService customerService) {
+		this.customerService = customerService;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DataPersistenceAndAccessPostgresqlApplication.class, args);
-		PostgradDAO postgradDAO = new PostgradDAO();
-		postgradDAO.test();
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.print("Enter the Customer ID: ");
+		int id = scanner.nextInt();
+		scanner.nextLine();
+		Customer customer = customerService.findById(id);
+		if (customer != null) {
+			System.out.println(customer);
+		} else {
+			System.out.println("Customer not found");
+		}
 	}
 }
