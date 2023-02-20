4b566cb4452595fcb62b38301b076daecae96b35
Arif Hossain <hossaininfo@ymail.com>
2023-02-20 16:36:54 +0100

    created Customer.java

---
+++ data_persistence_and_access_postgresql/src/main/java/com/example/data_persistence_and_access_postgresql/model/Customer.java
@@ -0,0 +1,16 @@
+package com.example.data_persistence_and_access_postgresql.model;
+
+public class Customer {
+    private int id;
+    private String first_name;
+    private String last_name;
+    private String company;
+    private String address;
+    private String city;
+    private String state;
+    private String country;
+    private String postal_code;
+    private String phone;
+    private String fax;
+    private String email;
+}
